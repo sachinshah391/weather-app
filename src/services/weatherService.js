156f17f8c3c5b028e751e92e286fbbe6a66fd917
Sachin Shah <sachin.shah@external.danone.com>
2023-04-05 22:15:59 +0530

    refinement2

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,7 +3,6 @@ import { DateTime } from "luxon";
 
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log('API value',API_KEY)
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 
